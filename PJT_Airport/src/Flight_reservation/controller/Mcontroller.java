44dda6192061b06c38f7c651e0b82fba3fa50550
Jun <amares9100@gmail.com>
2023-02-28 17:44:56 +0900

    02/28 로그인/회원가입/아이디찾기/비밀번호찾기/회원탈퇴

--- PJT_Airport/src/Flight_reservation/controller/Mcontroller.java
+++ PJT_Airport/src/Flight_reservation/controller/Mcontroller.java
@@ -1,6 +1,7 @@
 package Flight_reservation.controller;
 
 import Flight_reservation.model.Mdao;
+import Flight_reservation.model.Member;
 
 public class Mcontroller {
 	private static Mcontroller mcontroller = new Mcontroller();
@@ -13,28 +14,52 @@ public class Mcontroller {
 	public int login( String mid , String mpw  ) {
 		/* -> 로그인 성공 시 사용자 메인이동 // 관리자 아이디 로그인시 관리자 페이지 이동*/
 		int loginsession = Mdao.getInstance().login(mid, mpw);
+		if(loginsession == 1) { 
+			return 1; // 관리자 반환
+		}
+		else if(loginsession > 1) {
+			return 2; // 일반 로그인
+		}
+		else if(loginsession == 0) {
+			return 0;// 해당없으면 0 반환
+		}
+		else {return -1;}
 		
-		
-		
-		
-		
-		return 0; 
 	}
 	
 	//회원가입
-	public void signup() {
+	public boolean signup(String mid , String mpw , String mname , String mphone) {
 		
+		boolean result = Mdao.getInstance().idCheck(mid); // 중복 아이디 체크
+		 if(result) { // 아이디가 중복일때
+			 return false;
+		 }
+		 else {		// 중복이 아니면 회원가입 실행
+			Member memberdto =  new Member(mid , mpw , mname , mphone); // 입력값 객체화
+			
+			return Mdao.getInstance().signup(memberdto); // 리턴값 그대로 리턴
+		 }
 	}
 	
 	//아이디 찾기
-	public void findid() {
+	public String findid(String mname , String mphone) {
 		
+		return Mdao.getInstance().findid(mname, mphone);
 	}
 	
 	//비밀번호 찾기
-	public void findpw() {
+	public String findpw(String mid , String mname , String mphone) {
+		
+		return Mdao.getInstance().findpw(mid, mname , mphone);
+	}
+	
+	//회원탈퇴
+	public boolean deleteId(String mid , String mpw , String mname , String mphone) {
+		Member memberdto =  new Member(mid , mpw , mname , mphone);
+		return Mdao.getInstance().deleteId(memberdto);
 		
 	}
+	
 	
 	//예약 내역 확인
 	public void Myreser() {
